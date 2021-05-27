73d05689516259fcbdb2a723b5f22eb7ee467ad7
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-27 14:57:41 +0200

    complete ex

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,21 @@
-let root= new Vue ({
-    el: "#root",
-    data:{},
-    methods:{},
-    mounted(){}
-})
+var root = new Vue(
+    {
+        el: '#root',
+        data: {
+            emailList: [],
+        },
+        methods: {
+        },
+        mounted() {
+            for(let i = 0; i < 10; i++) {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response)=> {
+                    this.email = response.data.response;
+                    this.emailList.push(this.email);
+                });
+            }
+            
+        },
+    }
+);
